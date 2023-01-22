45ff7544571c591dca6048b60e09a87a0c2f4507
Julien Hu <hujulien123@gmail.com>
2023-01-21 22:58:26 -0500

    Link Not Implemented page to storage

--- public/js/notimplemented.js
+++ public/js/notimplemented.js
@@ -1,5 +1,5 @@
 const homeButton = document.getElementById("home-button");
 
 homeButton.onclick = () => {
-    window.location.href = "./index.html";
+    window.location.href = "./storage.html";
 };
