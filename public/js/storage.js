39b1bb467314686891e460dbeeef323af063f750
krishnacheemalapati <krishkrishct@gmail.com>
2023-01-22 04:35:13 -0500

    Added Chessboard

--- public/js/storage.js
+++ public/js/storage.js
@@ -25,3 +25,19 @@ container.addEventListener('click', function(e){
         hideSidebar();
     }
 }, true);
+
+var config = {
+    showNotation: false,
+    position: 'start',
+    draggable: true,
+    dropOffBoard: 'snapback',
+    onDrop: onDrop,
+  }
+var board = Chessboard('board', config)
+function onDrop (source, target, piece, newPos, oldPos, orientation) {
+    const target = target;
+    const piece = piece;
+    const stringPiece = 
+    console.log('Target: ' + target)
+    console.log('Piece: ' + piece)
+  }
