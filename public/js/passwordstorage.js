2bbf78e890db6e836e512be544a87de3cef82996
z1chh <lolchen2012@gmail.com>
2023-01-22 04:33:08 -0500

    hi

--- public/js/passwordstorage.js
+++ public/js/passwordstorage.js
@@ -1,27 +1,34 @@
-var container = document.querySelector('.container');
-var main = document.querySelector('.main');
+var container = document.querySelector(".container");
+var main = document.querySelector(".main");
 
-function toggleSidebar(){
-    isShowingSidebar() ? hideSidebar() : showSidebar();
+function toggleSidebar() {
+  isShowingSidebar() ? hideSidebar() : showSidebar();
 }
 
-function showSidebar(){
-    container.classList.add('show-sidebar');
+function showSidebar() {
+  container.classList.add("show-sidebar");
 }
 
-function hideSidebar(){
-    container.classList.remove('show-sidebar');
+function hideSidebar() {
+  container.classList.remove("show-sidebar");
 }
 
-function isShowingSidebar(){
-    return container.classList.contains('show-sidebar');
+function isShowingSidebar() {
+  return container.classList.contains("show-sidebar");
 }
 
-document.querySelector('.hamburger').addEventListener('click', toggleSidebar, false);
-
-container.addEventListener('click', function(e){
-    if(isShowingSidebar() && main.contains(e.target)){
-        e.preventDefault();
-        hideSidebar();
-    }
-}, true);
+function showicon() {
+  var t = document.getElementsByClassName("line3");
+  console.log(t);
+  for(let i = 0; i < t.length; i++){
+  if (t[i].value == "YES") {
+    t[i].value = "NO";
+    t[i].classList.remove("yesvalue");
+    t[i].classList.add("novalue");
+  } else {
+    t[i].value = "YES";
+    t[i].classList.remove("novalue");
+    t[i].classList.add("yesvalue");
+  }
+}
+}
